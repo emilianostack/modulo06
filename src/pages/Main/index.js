d4921786317890471b329aba0541413562848f74
Emiliano <emilianousb@gmail.com>
2019-10-13 20:36:50 -0300

    update

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -18,11 +20,37 @@ import {
 } from './styles';
 
 export default class Main extends React.Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     newUser: '',
     users: [],
     loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const { users, newUser } = this.state;
@@ -44,6 +72,14 @@ export default class Main extends React.Component {
       loading: false,
     });
     Keyboard.dismiss();
+  };
+
+  handleAddUser = async () => {};
+
+  handleNavigation = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
   };
 
   render() {
@@ -79,7 +115,7 @@ export default class Main extends React.Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigation(item)}>
                 <ProfileButtonText>Ver Perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -89,7 +125,3 @@ export default class Main extends React.Component {
     );
   }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
